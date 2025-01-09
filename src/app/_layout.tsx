85bbbc7492306a5467e17f8df09be26da7e954fc
matheushenrique <matheus.henriquepr@gmail.com>
2025-01-09 00:48:57 -0300

    add places

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,9 +6,10 @@ import {
   Rubik_500Medium,
   Rubik_700Bold,
  } from "@expo-google-fonts/rubik";
- import { colors } from "@/styles/theme";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
- import { Loading } from "@/components/loading";
+import { colors } from "@/styles/theme";
+import { Loading } from "@/components/loading";
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -23,11 +24,13 @@ export default function Layout() {
   }
 
   return (
-    <Stack 
-      screenOptions={{ 
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.gray[100] },
-      }}
-    />
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Stack 
+        screenOptions={{ 
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.gray[100] },
+        }}
+      />
+    </GestureHandlerRootView>
   )
 }
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -2,10 +2,17 @@ import { View, Alert } from "react-native";
 import { useEffect, useState } from "react";
 import { api } from "@/services/api";
 import { Categories, CategoriesProps } from "@/components/categories";
+import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
+
+type MarketsProps = PlaceProps & {
+  
+}
 
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([])
   const [category, setCategory] = useState("")
+  const [markets, setMarkets] = useState<MarketsProps[]>([])
 
   async function fetchCategories() {
     try {
@@ -17,14 +24,39 @@ export default function Home() {
     }
   }
 
+  async function fetchMarkets() {
+    try {
+      if (!category) {
+        return
+      }
+
+      const { data } = await api.get("/markets/category/" + category)
+      setMarkets(data);
+    } catch (error) {
+      Alert.alert("Markets", "Unable to load the markets.")
+    }
+  }
+
   useEffect(() => {
     fetchCategories()
   }, [])
+
+  useEffect(() => {
+    fetchMarkets()
+  }, [category])
+  
   
 
   return (
     <View style={{ flex: 1 }}>
-      <Categories data={categories} onSelect={setCategory} selected={category}/>
+      <Categories 
+        data={categories} 
+        onSelect={setCategory} 
+        selected={category}
+      />
+      <Places 
+        data={markets}
+      />
     </View>
   )
 }
